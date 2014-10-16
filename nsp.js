71f179e75e034d13bfd3f8aeee4f0befdea7ca5f
C Borkert <success@dpsw.info>
2014-10-15 20:43:12 -0500

    fix

--- nsp.js
+++ nsp.js
@@ -1,10 +1,18 @@
-var cache = {};
+var cache = {}, fs = require('fs'), debug = process.env.debug;
 
 function handle(req, res){
-	var fs = require('fs');
+	req.url = req.url.replace(/\.\./g,'.');
+	
+	if(req.url === '/') req.url = '/index.html'; 
+	
+	debug && console.log(req.url);
+	
+	//if(!req.url.match(/\.html$/) && fs.existsSync(req.url))
+	//  return res.sendFile(req.url);
+
 	try{
 		res.writeHead(200, {'Content-Type': 'text/html'});
-	var k = '.' + req.url;
+	var k = process.cwd() + req.url;
 	if(!cache[k]) cache[k] = fs.readFileSync(k).toString();
 	var t = cache[k].replace(/<\?\=?([\s\S]*?)\?>/g, function(m, p){
 		var x = eval(p);
@@ -18,5 +26,7 @@ function handle(req, res){
 }
 
 module.exports = function(port, ip){
-  require('http').createServer(handle).listen(port, ip);
+  require('http').createServer(handle).listen(port||80, ip);
+  //.listen(port||80, ip);
+  debug && console.log('started');
 }
